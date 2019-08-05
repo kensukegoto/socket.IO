74356658b5f989f1fbad9592e036466d36d8476e
kensukegoto <utogoken2@yahoo.co.jp>
2019-08-05 13:08:58 +0900

    [追加]簡易的なチャットルーム

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# socket.io
+# Namespaces
 
-サーバー側とブラウザ側それぞれのライブラリがある。お互いにライブラリを使う事でwebsockeでの通信が可能となる。<br>
-**すべてのブラウザ・モバイルデバイスでwebsocke通信を可能にするために作られている。**
+namespaceを使用すると複数のエンドポイントを作れる
 
-# 気づいた
+## 特定のチャンネルにいる人に発火
 
-- sourcemapが404エラー
-- sourcemapはコンソールを開いた際に取りに行かれる
+io.sockets.emit('hi', 'everyone');
+
+## すべてのチャンネルのいる人に発火
+
+io.emit('hi', 'everyone');
+
+## socket.broadcat()は自分以外全員
+
+## io.emit()は自分含め全員
+
+## namespaceに関係なく全員にメッセージを送る方法はある？
--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const nsp = io.of("/my-namespace");
+const nsp2 = io.of("/my-namespace2");
 
 app.get("/",(req,res)=>{
   res.sendFile(__dirname + '/index.html');
+});
+app.get("/2",(req,res)=>{
+  res.sendFile(__dirname + '/index2.html');
 });
 app.get("/socket.io.js",(req,res)=>{
   res.sendFile(__dirname + '/socket.io.js');
 });
 
-let users = {};
+let numUsers = 0;
 
-io.on("connection",socket => {
-
-  if(!users[socket.id]){
-    users[socket.id] = "名無し"
+nsp.on("connection",socket => {
+  numUsers++;
+  if(numUsers%2!==0){
+    socket.join("some room");
   }
-  socket.on("chat message",msgObj => {
-    const {m,user} = msgObj;
-    if(user) users[socket.id] = user;
-    io.emit("chat message",{user: users[socket.id],message: m});
-  });
-
+  
+  nsp.emit("hi","everyone");
+  nsp.to("some room").emit("hi","everyone in some room");
 });
+
 
 http.listen(3000,()=>{
   console.log("listening on *:3000");
