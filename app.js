ffea90275f4940721e8d0fec90424bfad018c0d6
kensukegoto <utogoken2@yahoo.co.jp>
2019-08-05 23:47:35 +0900

    [追加]選択したチャットルームに入出

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ app.use(express.static(path.join(__dirname,"public")));
 http.listen(port,()=>{
   console.log(`Server listening at port %d`,port);
 });
+
+let userCount = 0;
+
+io.on("connection",socket => {
+
+  userCount++;
+
+  let defaultRoom = null;
+  let currentRoom = null;
+
+  // join roomを受信
+  socket.on("join room",data => {
+
+    // デフォルトを保存
+    if(!defaultRoom) defaultRoom = Object.keys(socket.rooms)[0];
+
+    if(currentRoom === data.room) return;
+    
+    currentRoom = data.room;
+    socket.join(currentRoom);
+    io.to(currentRoom).emit("join",`ユーザーナンバー${userCount}さん`);
+    
+  })
+})
